1aaec7414dd2170380cf612fe8a692af05322153
Joaquin Incio <joaquin.incio2002@outlook.com>
2023-06-26 23:19:16 -0500

    Trabajando en lavandería 2

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float followSpeed;
-    public Transform target;
+    [SerializeField] private float followSpeed;
+    [SerializeField] private Transform target;
+    private Vector3 initialPostion;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPostion = transform.position - target.position;
     }
 
     // Update is called once per frame
     void Update()
     {   
-        Vector3 newPos = new Vector3(target.position.x, 2.5f, (target.position.z - 4.5f));
+        Vector3 newPos = new Vector3(target.position.x, initialPostion.y, (target.position.z + initialPostion.z));
         transform.position = Vector3.Slerp(transform.position, newPos, followSpeed);
     }
 }
