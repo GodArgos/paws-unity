c8ff348b7584b1e8dfb541baf8938040df3ae209
Joaquin Incio <joaquin.incio2002@outlook.com>
2023-07-07 00:03:35 -0500

    Comienzo edit tuto

--- Assets/Scripts/TutorialScripts/WASD_Trigger.cs
+++ Assets/Scripts/TutorialScripts/WASD_Trigger.cs
@@ -6,15 +6,29 @@ using UnityEngine.EventSystems;
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] private PlayerController playerController;
+    [SerializeField] private GameObject sprite;
+    [SerializeField] private Material dissolvingMaterial;
+    [SerializeField] private float dissolveAmount;
+
+    private Renderer renderer;
+
+    private void Start()
+    {
+        renderer = sprite.GetComponent<Renderer>();
+    }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (playerController.moveDirection.sqrMagnitude > 0.01f)
+            /*if (playerController.moveDirection.sqrMagnitude > 0.01f)
             {
                 
-            }
+            }*/
+
+            renderer.material = dissolvingMaterial;
+            renderer.material.SetFloat("_Dissolve", dissolveAmount);
+
         }
     }
 }
